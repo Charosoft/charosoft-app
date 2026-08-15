67d2d923d48938480d287d73362dfcfa1f148a24
charosoft <jenoviekola1@gmail.com>
2026-08-15 14:01:17 +0100

    Fix: Correction du nav

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -77,6 +77,19 @@ const About: React.FC = () => {
 
         </div>
       </div>
+          <a
+    id="cy-effective-orcid-url"
+    class="underline"
+     href="https://orcid.org/0009-0009-6680-4144"
+     target="orcid.widget"
+     rel="me noopener noreferrer"
+     style="vertical-align: top">
+     <img
+        src="https://orcid.org/sites/default/files/images/orcid_16x16.png"
+        style="width: 1em; margin-inline-start: 0.5em"
+        alt="ORCID iD icon"/>
+      https://orcid.org/0009-0009-6680-4144
+    </a>
     </div>
   );
 };
